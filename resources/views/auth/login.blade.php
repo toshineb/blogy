14a9afd2ccc7e1d4a71c3b7e2ebe233c59195381
TOSIN BELLO <106435635+toshineb@users.noreply.github.com>
2023-09-25 14:44:12 +0100

    Add files via upload

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+
+            <div class="comment-form-wrap pt-5">
+                <h3 class="mb-5">Login</h3>
+            <form action="{{ route('login') }}" method="POST" class="p-5 bg-light">
+            @csrf
+                <div class="form-group">
+                    <label for="email">Email *</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="website">Password</label>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Login</button>
+
+                </div>
+                            <!-- @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                            @endif -->
+            </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
